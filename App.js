d02b1120720bf35169590b6f2cb4f27b21ee9a32
Nubaid123 <09568@bpsdoha.edu.qa>
2024-01-27 09:11:30 +0300

    Stellar

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native'
+import Home from "screens\Home.js"
+import DailyPic from "screens/DailyPic"
+import SpaceCrafts from "screens/SpaceCrafts"
+import StarMap from "screens/StarMap"
+import {createStackNavigator} from '@react-navigation/stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Home Screen" component={Home}/>
+      <Stack.Screen name="Daily Pics" component={DailyPic}/>
+      <Stack.Screen name="Space Crafts" component={SpaceCrafts}/>
+      <Stack.Screen name="Star Maps" component={StarMap}/>
+      </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
